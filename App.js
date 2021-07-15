35a2d1cb2501b99a5429c79ae663dd9af71d3701
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2021-07-15 17:55:56 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import { StyleSheet, Text, View, createAppContainer, Image } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import WriteStoryScreen from './screens/WriteStoryScreen'
 import ReadStoryScreen from './screens/ReadStoryScreen'
 
